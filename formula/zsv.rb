52df9147f9cdda260778b531db7f9a263a90884c
liquidaty/zsv CI <liquidaty/zsv-ci@localhost>
2022-12-05 04:45:42 +0000

    Update liquidaty/homebrew-zsv tap.

--- formula/zsv.rb
+++ formula/zsv.rb
@@ -1,10 +1,10 @@
 class Zsv < Formula
   desc "zsv+lib: world's fastest (simd) CSV parser, with an extensible CLI"
   homepage 'https://github.com/liquidaty/zsv'
-  url 'https://github.com/liquidaty/zsv/releases/download/v0.1.0-alpha/zsv-0.1.0-alpha-amd64-macosx-gcc.tar.gz'
+  url 'https://github.com/liquidaty/zsv/releases/download/v0.2.1-alpha/zsv-0.2.1-alpha-amd64-macosx-gcc.tar.gz'
   head 'https://github.com/liquidaty/zsv.git', branch: 'main'
   license 'MIT'
-  sha256 '95ede1fa1299f9d18b6b45b3adb6cbf784b8de03aa182404b146b63d65cca34c'
+  sha256 '29e17e4aa57a0a47379ce22f85bd61a6aedc49fa1aebb723a1c016257c0f3443'
 
   def install
     bin.install 'bin/zsv'
