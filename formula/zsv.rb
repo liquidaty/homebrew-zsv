6a5a3289d4bb8c93dd655bbb9173552f2b77c7c7
zsv-ci <zsv-ci@localhost>
2024-12-16 07:25:52 +0000

    Automatic bump version to v0.4.1-alpha.

--- formula/zsv.rb
+++ formula/zsv.rb
@@ -1,10 +1,10 @@
 class Zsv < Formula
   desc "zsv+lib: world's fastest (simd) CSV parser, with an extensible CLI"
   homepage 'https://github.com/liquidaty/zsv'
-  url 'https://github.com/liquidaty/zsv/releases/download/v0.4.0-alpha/zsv-0.4.0-alpha-amd64-macosx-gcc.tar.gz'
+  url 'https://github.com/liquidaty/zsv/releases/download/v0.4.1-alpha/zsv-0.4.1-alpha-amd64-macosx-gcc.tar.gz'
   head 'https://github.com/liquidaty/zsv.git', branch: 'main'
   license 'MIT'
-  sha256 '1e4d08438bb1a64f1d7359869c24713796343342514fa93f57ab7850150c0f4a'
+  sha256 'c69ab5cb9c17f234c5936af60469ce83e0be01c1c229bd7255776aabc2d17568'
 
   def install
     bin.install 'bin/zsv'
