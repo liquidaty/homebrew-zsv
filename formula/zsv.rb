9bc7f67dd30aea85500eb77768db6b4c416f07d4
zsv-ci <zsv-ci@localhost>
2024-12-19 06:48:37 +0000

    Automatic bump version to v0.4.3-alpha.

--- formula/zsv.rb
+++ formula/zsv.rb
@@ -4,11 +4,11 @@ class Zsv < Formula
   head 'https://github.com/liquidaty/zsv.git', branch: 'main'
   license 'MIT'
 
-  AMD64_URL = 'https://github.com/liquidaty/zsv/releases/download/v0.4.1-alpha/zsv-0.4.1-alpha-amd64-macosx-gcc.zip'
-  AMD64_HASH = '3575ae51d6009f8ffb3a47fd371b8c3faf8fac1647b005cc96bebb09f27ad926'
+  AMD64_URL = 'https://github.com/liquidaty/zsv/releases/download/v0.4.3-alpha/zsv-0.4.3-alpha-amd64-macosx-gcc.zip'
+  AMD64_HASH = 'f7bae46a84307648d4a12da82d037c4a89c4c3b19d66f9b9dc7f82be18031290'
 
-  ARM64_URL = 'https://github.com/liquidaty/zsv/releases/download/v0.4.1-alpha/zsv-0.4.1-alpha-arm64-macosx-gcc.zip'
-  ARM64_HASH = '4b28dbc5bf58e432aa8327bfcd20101c776d1b032eb928f6c0d9d985a6821b02'
+  ARM64_URL = 'https://github.com/liquidaty/zsv/releases/download/v0.4.3-alpha/zsv-0.4.3-alpha-arm64-macosx-gcc.zip'
+  ARM64_HASH = '304149a42d6dbd9d57c7c2828ac54c32dd43b2a433fa105721040d61e420312a'
 
   if OS.mac?
     if Hardware::CPU.intel?
