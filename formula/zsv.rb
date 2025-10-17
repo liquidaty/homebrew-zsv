62363fc0cf2c4ae76bc808e23e99e94174f5e02e
Azeem Sajid <azeem.sajid@gmail.com>
2025-10-17 10:58:47 +0500

    Update formula with audit changes

--- formula/zsv.rb
+++ formula/zsv.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
 class Zsv < Formula
   desc "zsv: tabular data swiss-army knife CLI"
   homepage "https://github.com/liquidaty/zsv"
   license "MIT"
   head "https://github.com/liquidaty/zsv.git", branch: "main"
 
-  AMD64_URL = 'https://github.com/liquidaty/zsv/releases/download/v1.0.0/zsv-1.0.0-amd64-macosx-gcc.tar.gz'
-  AMD64_HASH = 'e7e79e7b76b8f154d43190b792a944ac61e0ca0b0def4d23eaff5470f1d64f90'
+  AMD64_URL = "https://github.com/liquidaty/zsv/releases/download/v1.0.0/zsv-1.0.0-amd64-macosx-gcc.tar.gz"
+  AMD64_HASH = "e7e79e7b76b8f154d43190b792a944ac61e0ca0b0def4d23eaff5470f1d64f90"
 
-  ARM64_URL = 'https://github.com/liquidaty/zsv/releases/download/v1.0.0/zsv-1.0.0-arm64-macosx-gcc.tar.gz'
-  ARM64_HASH = '42b6ce98654f87a01ae31579749a786d4cd50857f2475bcb053650e229f7d617'
+  ARM64_URL = "https://github.com/liquidaty/zsv/releases/download/v1.0.0/zsv-1.0.0-arm64-macosx-gcc.tar.gz"
+  ARM64_HASH = "42b6ce98654f87a01ae31579749a786d4cd50857f2475bcb053650e229f7d617"
 
-  if OS.mac?
-    if Hardware::CPU.intel?
+  on_macos do
+    on_intel do
       url AMD64_URL
       sha256 AMD64_HASH
-    elsif Hardware::CPU.arm?
+    end
+    on_arm do
       url ARM64_URL
       sha256 ARM64_HASH
     end
