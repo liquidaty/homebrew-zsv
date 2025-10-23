a664b8d3d97dd08c19b1301628d1af1693773d8f
zsv-ci <zsv-ci@localhost>
2025-10-23 03:22:32 +0000

    Automatic bump version to v1.0.1.

--- formula/zsv.rb
+++ formula/zsv.rb
@@ -6,11 +6,11 @@ class Zsv < Formula
   license "MIT"
   head "https://github.com/liquidaty/zsv.git", branch: "main"
 
-  AMD64_URL = "https://github.com/liquidaty/zsv/releases/download/v1.0.0/zsv-1.0.0-amd64-macosx-gcc.tar.gz"
-  AMD64_HASH = "e7e79e7b76b8f154d43190b792a944ac61e0ca0b0def4d23eaff5470f1d64f90"
+  AMD64_URL = 'https://github.com/liquidaty/zsv/releases/download/v1.0.1/zsv-1.0.1-amd64-macosx-gcc.tar.gz'
+  AMD64_HASH = '0870de5b4a43b1e902dc78051dae68549a39b5c52e54b1a5316f541da8efe1ea'
 
-  ARM64_URL = "https://github.com/liquidaty/zsv/releases/download/v1.0.0/zsv-1.0.0-arm64-macosx-gcc.tar.gz"
-  ARM64_HASH = "42b6ce98654f87a01ae31579749a786d4cd50857f2475bcb053650e229f7d617"
+  ARM64_URL = 'https://github.com/liquidaty/zsv/releases/download/v1.0.1/zsv-1.0.1-arm64-macosx-gcc.tar.gz'
+  ARM64_HASH = '79c93751b8e21bc2869eae51a9a548aaa1c0e4d83af20f4e5cfc86f0c3afb1d5'
 
   on_macos do
     on_intel do
